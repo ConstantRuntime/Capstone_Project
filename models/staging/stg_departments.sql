ef51f4a938aaf22673ce1890a6946b26981e1752
O'Shaun Berridge <oshaun1250@gmail.com>
2023-09-06 21:58:51 +0000

    Staged departments

--- models/staging/stg_departments.sql
+++ models/staging/stg_departments.sql
@@ -1,1 +1,3 @@
-select * from raw.walmart_sales.departments
+-- select * from raw.walmart_sales.departments;
+
+SELECT DEPT as DEPT_ID from raw.walmart_sales.departments, STORE as STORE_ID from raw.walmart_sales.departments;

d483f76b8b18d9de56f88cc5d9b8f7eb37d3491c
Nicolas Nunez Baez <nicolas.y.nunezbaez@gmail.com>
2023-09-06 23:43:45 +0000

    staged stores and conditions

--- models/staging/stg_conditions.sql
+++ models/staging/stg_conditions.sql
@@ -1,1 +1,14 @@
-select * from raw.walmart_sales.conditions
+SELECT 
+    store as store_id,
+    date,
+    temperature,
+    fuel_price,
+    mark_down_1,
+    mark_down_2,
+    mark_down_3,
+    mark_down_4,
+    mark_down_5,
+    cpi,
+    unemployment,
+    is_holiday
+FROM raw.walmart_sales.conditions
